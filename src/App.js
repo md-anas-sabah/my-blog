c35c5864f11439ded1483499ce29d309d5c9638b
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-31 21:50:20 +0530

    update: added react-router-dom and body.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Header from './component/Header';
 import './App.css';
 import Banner from './component/Banner';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Body from './component/Body';
+
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Body />,
+  }
+])
+
 
 function App() {
   return (
     <div className=''>
       <Header />
       <Banner />
+      <RouterProvider router={appRouter} />
     </div>
   );
 }
---
+++ src/component/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='px-10 py-5'>
+        <h1 className='text-7xl'>Coming Soon🚀</h1>
+    </div>
+  )
+}
+
+export default Body
