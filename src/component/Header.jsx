01fb9e78e3199ec5f0f726f18eae58a5b3f492da
Md Anas Sabah <mdanassabah@gmail.com>
2023-03-31 21:30:17 +0530

    update: minor ui change

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,9 +5,9 @@ const Header = () => {
       <header className="flex items-center justify-between space-x-2 font-bold px-10 py-5">
           <div className="flex items-center space-x-2 md:text-2xl md:tracking-wide">
               <a href="/">
-                 <img src={Logo} alt="logo" className="rounded-full  h-16 w-16" /> 
+                 <img src={Logo} alt="logo" className="rounded-full  h-14 w-14" /> 
               </a>
-              <h1 className="font-zhi">Md Anas Sabah</h1>
+              <h1 className="font-zhi text-base">Md Anas Sabah</h1>
           </div>
           <div>
           <a href="https://www.linkedin.com/in/md-anas-sabah/" className="px-5 py-3 text-sm md:text-base bg-gray-900 flex items-center rounded-full text-center text-[#F7AB0A]">
