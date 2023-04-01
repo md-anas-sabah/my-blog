cbb97f1c37265b876673968ea96828faf08da174
Md Anas Sabah <mdanassabah@gmail.com>
2023-04-01 20:03:02 +0530

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const appRouter = createBrowserRouter([
 
 function App() {
   return (
-    <div className=''>
+    <div className=' md:w-[100rem] md:ml-auto md:mr-auto'>
       <Header />
       <Banner />
       <RouterProvider router={appRouter} />
--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -14,4 +14,4 @@ const Banner = () => {
     )
   }
   
-  export default Banner
+  export default Banner;
