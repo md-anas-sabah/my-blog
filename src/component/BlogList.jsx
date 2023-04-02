6a8ff0c3f7f1860bb16dbc62c6e15b9d492bccda
Md Anas Sabah <mdanassabah@gmail.com>
2023-04-02 14:36:46 +0530

    update: mobile responsive

--- src/component/BlogList.jsx
+++ src/component/BlogList.jsx
@@ -5,12 +5,30 @@ import { Link } from 'react-router-dom'
 const BlogList = () => {
   return (
     <div className='px-10 py-5'>
-         <hr className="border-[#F7AB0A] mb-10"  />
-         <div>
-            <Link to="www.github.com">
+        <div>
+           <hr className="border-[#F7AB0A] mb-10" />
+        </div>
+         
+         <div className='flex flex-col items-center md:flex-col md:items-center' >
+            <div className='flex flex-col md:flex-row md:flex-wrap md:justify-between' >
+             <Link to="www.github.com">
                 <Blogs title="NextJS: Next-Auth" image="https://res.cloudinary.com/practicaldev/image/fetch/s--n8wCV2Oa--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ul5mxl9hasuvd9jnh51f.jpg" createdAt="14 Oct 2022" category_1="NextJS" category_2="Dev.to"/> 
             </Link>
+            <Link to="www.github.com">
+                <Blogs title="React + Redux" image="https://res.cloudinary.com/practicaldev/image/fetch/s--tcZq71xv--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ulbh4rjjxhxz4ynowygh.jpg" createdAt="19 July 2022" category_1="React" category_2="Redux"/> 
+            </Link>
+            <Link to="www.github.com">
+                <Blogs title="Life Cycle of a Component" image="https://res.cloudinary.com/practicaldev/image/fetch/s--RDdxoyBw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/g1gg7j4clkbh0i84eka7.png" createdAt="08 July 2022" category_1="React" category_2="Dev.to"/> 
+            </Link>
+            <Link to="www.github.com">
+                <Blogs title="TypeScript + React" image="https://res.cloudinary.com/practicaldev/image/fetch/s--X8DtWdxA--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/lk4xyn1glcyerqqs66xe.png" createdAt="28 June 2022" category_1="React" category_2="TypeScript"/> 
+            </Link>
+            </div>
+            <div className='px-5 py-3 text-[10px] md:text-base bg-gray-900 flex items-center justify-center  rounded-full text-[#F7AB0A] h-14 w-40'>
+            <Link to="/" className='text-lg'>See More</Link>
+            </div>
          </div>
+        
 
     </div>
   )
--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Blogs = ({image, title, createdAt, category_1, category_2}) => {
   return (
-    <div className="flex flex-col group cursor-pointer mb-10">
+    <div className="flex flex-col group cursor-pointer mb-10 max-w-2xl ">
         <div className="relative w-full drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
             <img src={image} alt='im' className="object-fill h-80 object-left lg:object-center" />
         <div className="absolute bottom-0 w-full bg-black bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-white flex flex-row justify-between ">
@@ -10,12 +10,12 @@ const Blogs = ({image, title, createdAt, category_1, category_2}) => {
                 <p className="font-bold text-2xl" >{title}</p>
                 <p className="text-base">{createdAt}</p>
             </div>
-            <div className='flex flex-col md:flex-row gap-y-1 px-2 mt-1 md:gap-x-2'>
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
-                    <p>{category_1}</p>
+            <div className='flex flex-col md:flex-row px-2 mt-1 mb-1 md:gap-x-2'>
+                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold md:h-10 md:w-20'>
+                    <p className='md:mt-1'>{category_1}</p>
                 </div>                 
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
-                    <p>{category_2}</p>    
+                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold md:h-10 md:w-20'>
+                    <p className='md:mt-1'>{category_2}</p>    
                 </div>   
              </div>
         </div>
