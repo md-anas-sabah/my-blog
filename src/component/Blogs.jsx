bf38dc6380ba3d1cc12a7162631e686e53b38cb1
Md Anas Sabah <mdanassabah@gmail.com>
2023-04-02 20:02:10 +0530

    update: fixing responsiveness bugs

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -7,15 +7,15 @@ const Blogs = ({image, title, createdAt, category_1, category_2}) => {
             <img src={image} alt='im' className="object-fill h-80 object-left lg:object-center" />
         <div className="absolute bottom-0 w-full bg-black bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-white flex flex-row justify-between ">
             <div>
-                <p className="font-bold text-2xl w-60 md:w-full truncate" >{title}</p>
+                <p className="font-bold text-2xl w-60 lg:w-full truncate" >{title}</p>
                 <p className="text-base">{createdAt}</p>
             </div>
-            <div className='flex flex-col md:flex-row px-2 mt-1 mb-1 md:gap-x-2'>
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold md:h-10 md:w-20'>
-                    <p className='md:mt-1'>{category_1}</p>
+            <div className='flex flex-col lg:flex-row px-2 mt-1 mb-1 lg:gap-x-2'>
+                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold lg:h-10 lg:w-20'>
+                    <p className='lg:mt-1'>{category_1}</p>
                 </div>                 
                 <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold md:h-10 md:w-20'>
-                    <p className='md:mt-1'>{category_2}</p>    
+                    <p className='lg:mt-1'>{category_2}</p>    
                 </div>   
              </div>
         </div>
