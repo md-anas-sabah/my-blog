6e545459b052934c850da06e5e779c001e12490c
Md Anas Sabah <mdanassabah@gmail.com>
2023-04-02 21:23:27 +0530

    update

--- src/component/Blogs.jsx
+++ src/component/Blogs.jsx
@@ -11,11 +11,11 @@ const Blogs = ({image, title, createdAt, category_1, category_2}) => {
                 <p className="text-base">{createdAt}</p>
             </div>
             <div className='flex flex-col lg:flex-row px-2 mt-1 mb-1 gap-y-1 lg:gap-x-2'>
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold lg:h-10 lg:w-20'>
-                    <p className='lg:mt-1'>{category_1}</p>
+                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20'>
+                    <p className='lg:mt-1 truncate'>{category_1}</p>
                 </div>                 
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold md:h-10 md:w-20'>
-                    <p className='lg:mt-1'>{category_2}</p>    
+                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20'>
+                    <p className='lg:mt-1 truncate'>{category_2}</p>    
                 </div>   
              </div>
         </div>
