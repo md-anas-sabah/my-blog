eb2db71e133eaee2b2609462a9628a16421bee6a
Md Anas Sabah <mdanassabah@gmail.com>
2023-07-08 01:41:27 +0530

    update

--- src/component/ProjectList.jsx
+++ src/component/ProjectList.jsx
@@ -1,42 +1,69 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Projects from './Projects'
-import muroexe from '../assets/muroexe.png'
-import vsapp from '../assets/vsapp.png'
-import challan from '../assets/challan.jpg'
-import foodista from '../assets/foodista.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import Projects from "./Projects";
+import muroexe from "../assets/muroexe.png";
+// import vsapp from "../assets/vsapp.png";
+import challan from "../assets/challan.jpg";
+import foodista from "../assets/foodista.png";
+import quiz from "../assets/quiz.png";
+import todo from "../assets/todo.png";
 
 const ProjectList = () => {
   return (
-    <div className='px-10 py-5'>
-        <div>
-            <hr className="border-[#F7AB0A] mb-10" />
-        </div>
-        
-        <div>
-            <h1 className='text-center text-3xl uppercase'>My Projects</h1>
-        </div>
-        <div className='flex flex-col mt-3 items-center md:flex-col md:items-center' >
-            <div className='flex flex-col md:flex-row md:flex-wrap md:justify-between' >
-             <Link to="www.github.com">
-                <Projects title="Muroexe" image={muroexe} createdAt="13 March 2023" category_1="React" category_2="Redux "/> 
-            </Link>
-            <Link to="www.github.com">
-                <Projects title="Video Streaming Application" image={vsapp} createdAt="01 March 2023" category_1="React" category_2="Redux"/> 
-            </Link>
-            <Link to="www.github.com">
-                <Projects title="Challan-X" image={challan}  createdAt="10 Nov 2023" category_1="React" category_2="MongoDB"/> 
-            </Link>
-            <Link to="www.github.com">
-                <Projects title="Foodista" image={foodista}  createdAt="10 January 2023" category_1="React" category_2="Context-Api"/> 
-            </Link>
-            </div>
-            <div className='px-5 py-3 text-[10px] md:text-base bg-gray-900 flex items-center justify-center  rounded-full text-[#F7AB0A] h-12 w-40'>
-            <Link to="/" className='text-lg'>See More</Link>
-            </div>
-         </div> 
-    </div>
-  )
-}
+    <div className="px-10 py-5">
+      <div>
+        <hr className="border-[#F7AB0A] mb-10" />
+      </div>
 
-export default ProjectList
+      <div>
+        <h1 className="text-center text-3xl uppercase">My Projects</h1>
+      </div>
+      <div className="flex flex-col mt-3 items-center md:flex-col md:items-center">
+        <div className="flex flex-col md:flex-row md:flex-wrap md:justify-between">
+          <Projects
+            title="Muroexe"
+            image={muroexe}
+            createdAt="13 March 2023"
+            category_1="https://github.com/md-anas-sabah/muroexe"
+            category_2="https://muroexe.vercel.app/ "
+          />
+
+          <Link to="https://otaku-quizzer.vercel.app/">
+            <Projects
+              title="Quiz Application"
+              image={quiz}
+              createdAt="01 March 2023"
+              category_1="React"
+              category_2="useReducer"
+            />
+          </Link>
+          <Link to="https://adv-todo-iota.vercel.app/">
+            <Projects
+              title="to-do plus pomodoro app"
+              image={todo}
+              createdAt="10 Nov 2023"
+              category_1="React"
+              category_2="MongoDB"
+            />
+          </Link>
+          <Link to="www.github.com">
+            <Projects
+              title="Foodista"
+              image={foodista}
+              createdAt="10 January 2023"
+              category_1="React"
+              category_2="Context-Api"
+            />
+          </Link>
+        </div>
+        <div className="px-5 py-3 text-[10px] md:text-base bg-gray-900 flex items-center justify-center  rounded-full text-[#F7AB0A] h-12 w-40">
+          <Link to="/" className="text-lg">
+            See More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectList;
--- src/component/Projects.jsx
+++ src/component/Projects.jsx
@@ -1,27 +1,39 @@
-import React from 'react'
-
-const Projects = ({image, title, createdAt, category_1, category_2}) => {
+import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkIcon from "@mui/icons-material/Link";
+import { Link } from "react-router-dom";
+const Projects = ({ image, title, createdAt, category_1, category_2 }) => {
   return (
     <div className="flex flex-col group cursor-pointer mb-10 max-w-2xl ">
-        <div className="relative w-full drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
-            <img src={image} alt='im' className="object-fill h-64 lg:h-80 md:w-[30rem] object-left lg:object-center" />
+      <div className="relative w-full drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+        <img
+          src={image}
+          alt="im"
+          className="object-fill h-64 lg:h-80 md:w-[30rem] object-left lg:object-center"
+        />
         <div className="absolute bottom-0 w-full bg-black bg-opacity-20 backdrop-blur-lg rounded drop-shadow-lg text-white flex flex-row justify-between ">
-            <div>
-                <p className="font-bold text-2xl w-52 lg:w-52 truncate" >{title}</p>
-                <p className="text-base">{createdAt}</p>
-            </div>
-            <div className='flex flex-col lg:flex-row px-2 mt-1 mb-1 gap-y-1 lg:gap-x-2'>
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20'>
-                    <p className='lg:mt-1 truncate'>{category_1}</p>
-                </div>                 
-                <div className='bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20'>
-                    <p className='lg:mt-1 truncate'>{category_2}</p>    
-                </div>   
-             </div>
+          <div>
+            <p className="font-bold text-2xl w-52 lg:w-52 truncate">{title}</p>
+            <p className="text-base">{createdAt}</p>
+          </div>
+          <div className="flex flex-col lg:flex-row px-2 mt-1 mb-1 gap-y-1 lg:gap-x-2">
+            <Link to={category_1}>
+              <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20">
+                {/* <p className='lg:mt-1 truncate'>{category_1}</p> */}
+                <GitHubIcon />
+              </div>
+            </Link>
+            <Link to={category_2}>
+              <div className="bg-[#F7AB0A] text-center text-black px-3 py-1 rounded-full text-sm font-semibold w-20 lg:h-10 lg:w-20">
+                {/* <p className="lg:mt-1 truncate">{category_2}</p> */}
+                <LinkIcon />
+              </div>
+            </Link>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
